5acab78ba9a36f1e195daca3c3b6156e6291fd90
ecabigting <ericcabigting@outlook.com>
2024-07-24 12:40:15 +0400

    update
    Former-commit-id: ad2332d871314ed699edb62019136d2f5552efcd [formerly 5854ccb4727ef229dfe25c9926319a4c818fc64b]
    Former-commit-id: 0fdb82a80f44575080f5948e8849a1bf24f36b88

---
+++ vmstart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+virsh --connect qemu:///system start win11-base
+sleep 30
+xfreerdp3 -grab-keyboard /v:192.168.122.225 /size:100% /u:user /p:pass /cert:ignore /d: /dynamic-resolution /gfx:avc444,progressive:on &
